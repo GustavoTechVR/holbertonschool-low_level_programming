dd0b13ead77aaf1ea74f2496bd779be581706976
GustavoTechVR <gjomequem@gmail.com>
2023-10-16 22:18:35 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - puts text
- * Description: puts 
- * Return: Always 0 (Success)
+ * Description: puts info
+ * Return: 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
