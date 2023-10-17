df4fdd6d7f472c08a41ad3fe41b2575807076377
GustavoTechVR <gjomequem@gmail.com>
2023-10-17 13:56:05 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include <stdio.h>
 
 /**
  * main - positive negativa
@@ -16,7 +16,7 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else
+	else if (n==0)
 	{
 		printf("%d is zero\n", n);
 	else
