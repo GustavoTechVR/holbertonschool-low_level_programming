b44489fef8a6bd865564b479db93ff5160d18008
GustavoTechVR <gjomequem@gmail.com>
2023-10-19 00:00:21 -0700

    task

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - void
+ * Description: task x10
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+
+	_putchar('\n');
+	}
+}
