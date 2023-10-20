0a5e4a32ea8e1a60d4171d90cc7807fce1a159c3
GustavoTechVR <gjomequem@gmail.com>
2023-10-19 23:43:56 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,17 +7,20 @@
  * Return: 0
  */
 
+void _putchar(char c);
+
 void print_number(int n)
 	{
-	if (n < 10)
-	{
-		_putchar(' ');
-	}
+	if (n =< 9)
+		{
+		_putchar(n + '0');
+		}
 	else
-	{
+		{
 		_putchar((n / 10) + '0');
-	}
-	_putchar((n % 10) + '0');
+	
+		_putchar((n % 10) + '0');
+		}
 	}
 
 void times_table(void)
