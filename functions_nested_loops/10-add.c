20e26ded20e4cac995688b010767e415401a9521
GustavoTechVR <gjomequem@gmail.com>
2023-10-20 00:40:53 -0700

    10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 
 /**
  * add - int
- * 
+ *
  * Description: adds two integers
  * Return: result
  */
@@ -10,5 +10,5 @@
 int add(int a, int b)
 {
 	int result = a + b;
-	return result;
+	return (result);
 }
