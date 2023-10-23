8be42ccff37733546fb8ef1c15e95db1ccf107c9
GustavoTechVR <gjomequem@gmail.com>
2023-10-22 19:56:29 -0700

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,6 +5,7 @@ void print_diagonal(int n)
 	if (n <= 0)
 		{
 		_putchar('\n');
+		}
 	else
 		{
 		int espacios = 0;
@@ -26,5 +27,5 @@ void print_diagonal(int n)
 			_putchar('\n');
 			}
 		}
-		}
+		
 	}
