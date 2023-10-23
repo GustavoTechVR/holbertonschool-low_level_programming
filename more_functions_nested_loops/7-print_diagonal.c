efe62f93169fc6319f9b97f1f2b8398c841bbead
GustavoTechVR <gjomequem@gmail.com>
2023-10-22 19:52:30 -0700

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void print_diagonal(int n)
+	{
+	if (n <= 0)
+		{
+		_putchar('\n');
+	else
+		{
+		int espacios = 0;
+		int diagonales = 0;
+
+		while (espacios < n)
+			{
+			for (int i = 0; i < espacios; i++)
+				{
+				_putchar(' ');
+				}
+			_putchar('\\');
+			_putchar('\n');
+			espacios++;
+			diagonales++;
+			}
+		if (n > 1)
+			{
+			_putchar('\n');
+			}
+		}
+		}
+	}
