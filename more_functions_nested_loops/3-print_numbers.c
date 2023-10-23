fd0f53980a9f3e3615522ece062f475d4f0a14e3
GustavoTechVR <gjomequem@gmail.com>
2023-10-22 18:21:41 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - void
+ * Description: prints the numbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		_putchar(numero + '0');
+		number++;
+	}
+	_putchar('\n');
+}
