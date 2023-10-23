ff75e3101ad90e39efc8c6e07ace9864c18d8e2a
GustavoTechVR <gjomequem@gmail.com>
2023-10-22 21:21:26 -0700

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,73 +15,23 @@ void print_to_98(int n)
 		{
 		for (i = n; i <= 98; i++)
 			{
-			if (i != 98)
-			{
-				if (n < 0) 
+			printf("%d", i);
+			if (i < 98)
 				{
-				_putchar('-');
-				_putchar(i / 10 * -1 + '0');
-				_putchar(i % 10 * -1 + '0');
+				printf(", ");
 				}
-			else
-				{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				}
-			_putchar(',');
-			_putchar(' ');
 			}
-		else	
-			{
-			if (n < 0)
-				{
-				_putchar('-');
-				_putchar(i / 10 * -1 + '0');
-				_putchar(i % 10 * -1 + '0');
-				}
-			else
-				{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				}
-			_putchar('\n');
-			}
-        		}
 		}
 	else
-		{
+	{
 		for (i = n; i >= 98; i--)
 			{
-			if (i != 98)
+			printf("%d", i);
+			if (i > 98)
 				{
-				if (n < 0)
-					{
-					_putchar('-');
-					_putchar(i / 10 * -1 + '0');
-					_putchar(i % 10 * -1 + '0');
-					}
-				else 
-					{
-					_putchar(i / 10 + '0');
-					_putchar(i % 10 + '0');
-					}
-				_putchar(',');
-				_putchar(' ');
-				}
-			else
-				{
-				if (n < 0) {
-					_putchar('-');
-					_putchar(i / 10 * -1 + '0');
-					_putchar(i % 10 * -1 + '0');
-				}
-				else
-				{
-					_putchar(i / 10 + '0');
-					_putchar(i % 10 + '0');
-				}
-				_putchar('\n');
+				printf(", ")
 				}
 			}
-		}
+	}
+	printf("\n");
 	}
