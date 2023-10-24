d075c49b8b9d107829e3f42c9c0881c7f3bdde6e
GustavoTechVR <gjomequem@gmail.com>
2023-10-23 21:21:55 -0700

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - int
+ * @n: the character checked
+ * Description: akes a pointer to an int as parameter and updates the value
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
