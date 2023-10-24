94ebe013af3c4ee0238078e61921c1924309f891
GustavoTechVR <gjomequem@gmail.com>
2023-10-24 15:02:43 -0700

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - char
+ * @s: checked
+ * Description: Write a function that prints a string
+ * Return: 0
+ */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')  // Caso base: cuando llegamos al final de la cadena
-	{
-		_putchar('\n');  // Imprimimos una nueva línea
+	if (*s == '\0')
+		{
+		_putchar('\n');
 		return;
 		}
 
