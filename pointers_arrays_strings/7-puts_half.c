08e78ef334bcc3d9987d96d4691a25d21713d283
GustavoTechVR <gjomequem@gmail.com>
2023-10-24 00:06:01 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ void puts_half(char *str)
 {
 	int length = 0;
 	int i;
-	
+
 	while (str[length] != '\0')
 	{
 		length++;
 	}
-	
+
 	i = (length - 1) / 2 + 1;
-	
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
