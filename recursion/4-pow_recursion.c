a83fc3c6b9e8af8821baeaf6bb79d071f5849669
GustavoTechVR <gjomequem@gmail.com>
2023-10-24 23:07:40 -0700

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,22 +5,18 @@
  * _islower - int
  * @x: the character checked
  * @y: checked
- * Description: Write a function that returns the value of x raised to the power of y
+ * Description: function returns the value of x raised to the power of y
  * Return: 0
  */
 
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-
-        	return (-1);
+		return (-1);
 
 	else if (y == 0)
-
-		return 1;
+		return (1);
 
 	else
-	
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 }
-
