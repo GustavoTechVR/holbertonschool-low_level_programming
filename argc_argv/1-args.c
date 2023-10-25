cfb65f45b85ec281fff4cfb31cdde7fc45baf23c
GustavoTechVR <gjomequem@gmail.com>
2023-10-25 10:05:04 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - int
+ *
+ * Description: Write a program that prints the number of arguments passed into it
+ * Return: 0
+ */
+
+int main(int , char *argv[])
+        {
+        printf("%s\n", argc - 1);
+        return (0);
+        }
