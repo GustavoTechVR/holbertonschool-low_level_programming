d6ecd19a14408e915e9566b45925543fcb603f36
GustavoTechVR <gjomequem@gmail.com>
2023-10-29 21:23:59 -0700

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,7 +2,9 @@
 
 /**
  * is_prime_number - int
+ * check_divisibility - int
  * @n: checked
+ * @i: checked
  * Description: verify prime number
  * Return: 1 prime number, otherwise 0
  */
@@ -11,23 +13,23 @@
 
 int is_prime_number(int n)
 {
-	int i;
-	
 	if (n <= 1)
-		return (0);
-	if (n <= 3)
-		return (1);
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	if (n % 6 != 1 && n % 6 != 5)
-		return 0;
-
-	for (i = 5; i * i <= n; i += 6)
 	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
+		return (0);
+	}
+	return (check_divisibility(n, 2));
+}
+
+int check_divisibility(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
 	}
 
-	return (1);
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (check_divisibility(n, i + 1));
 }
