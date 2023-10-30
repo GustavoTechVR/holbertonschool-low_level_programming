77718bbfd2162332f2ddef59353d3535b185a882
GustavoTechVR <gjomequem@gmail.com>
2023-10-29 21:40:38 -0700

    7 advanced v2

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -8,6 +8,8 @@
 
 int is_prime_number(int n)
 {
+	int i;
+	
 	if (n <= 1)
 		return (0);
 	if (n <= 3)
@@ -18,7 +20,7 @@ int is_prime_number(int n)
 	if (n % 6 != 1 && n % 6 != 5)
 		return (0);
 
-	for (int i = 5; i * i <= n; i += 6)
+	for (i = 5; i * i <= n; i += 6)
 	{
 		if (n % i == 0 || n % (i + 2) == 0)
 			return (0);
