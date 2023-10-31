a8f54d66e819244d7ae853be5723e0faab1f4fc1
GustavoTechVR <gjomequem@gmail.com>
2023-10-31 15:41:17 -0700

    Fix: header v2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,9 +19,8 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef struct dog dog_t;
-
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(const dog_t *d);
+/* dog_t *new_dog(char *name, float age, char *owner);
+ * void free_dog(dog_t *d);
+ */
 
 #endif
