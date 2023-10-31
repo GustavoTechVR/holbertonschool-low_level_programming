14194cd7f9d1a1e0c720b49200d05d084d465e69
GustavoTechVR <gjomequem@gmail.com>
2023-10-31 15:31:28 -0700

    Fix: header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+/**
+ * struct dog - struct for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return:
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
