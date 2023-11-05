d0c23a90352f24213e530ef1e1de934acdea5801
GustavoTechVR <gjomequem@gmail.com>
2023-11-05 14:21:55 -0800

    1. If you spend too much time thinking about a thing, you'll never get it done

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stddef.h> // Necesario para el tipo de datos size_t
+
+/**
+ * array_iterator - int
+ * @array: check array
+ * @size: check size
+ * @action: check action
+ * Description: parameter element array
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		for (size_t i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
