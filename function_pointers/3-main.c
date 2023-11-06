d12c5482b557cd51fbd6d9d6549fa4279268cc28
GustavoTechVR <gjomequem@gmail.com>
2023-11-05 16:01:44 -0800

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operator;
+	int (*calc_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	calc_func = get_op_func(operator);
+
+	if (calc_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = calc_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * op_add - int
+ * op_sub - int
+ * op_mul - int
+ * op_div - int
+ * op_mod - int
+ * @a: the character checked
+ * @b: the character checked
+ * Description: program that performs simple operations
+ * Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
